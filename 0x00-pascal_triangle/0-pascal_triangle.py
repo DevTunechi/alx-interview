656bdafc5d3644497ac9947b3e441122252a72d9
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-07-23 07:30:55 +0100

    ammends

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,14 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        row = [1]
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
+
     return triangle
+
