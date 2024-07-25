4c8c53b7ad76118a52d90f6f457f0f09856d12e3
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-07-25 16:22:22 +0100

    Second trial

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
 
 def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    x = []
     if n <= 0:
-        return []
-
-    triangle = []
-
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-
-    return triangle
-
+        return x
+    x = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(x[i - 1]) - 1):
+            curr = x[i - 1]
+            temp.append(x[i - 1][j] + x[i - 1][j + 1])
+        temp.append(1)
+        x.append(temp)
+    return x
