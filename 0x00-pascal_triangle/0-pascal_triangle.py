699194446365606f1cdb36f81e0324c82b931cb5
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-07-26 02:20:08 +0100

    Should work now

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
+""" Solving Pascal's Triangle """
+
+
+def Calc(row, col):
+    """ Recursive call to calculate rows and columns """
+    if col == 0 or col == row:
+        return 1
+    return Calc(row - 1, col - 1) + Calc(row - 1, col)
+
 
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing
-    the Pascal's Triangle of n
-    Returns an empty list if n <= 0
-    """
+    """ Calc the pascal triangle to the nth row """
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    
-    return triangle
+    trg = []
+    for i in range(n):
+        row = [Calc(i, j) for j in range(i + 1)]
+        trg.append(row)
+    return trg
