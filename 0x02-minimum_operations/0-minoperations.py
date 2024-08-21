313fdc1cf764f6f1e9d20a54e9fdfd1d80baba2b
DevTunechi <tunjidare2@yahoo.com>
2024-08-21 07:36:21 +0100

    Edits

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+""" Minimum Operations """
+
 
 def minOperations(n):
+    """
+        Calcs fewest ops num needed to result in n H chars
+    """
     if n <= 1:
         return 0
 
-    operations = 0
-    factor = 2
+    ops = 0
+    factorizer = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        if n % factorizer == 0:
+            ops += factorizer
+            n //= factorizer
+        else:
+            factorizer += 1
 
-    return operations
+    return ops
