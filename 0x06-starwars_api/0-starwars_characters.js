8644e7a74d6bb650b2e8be860aa24437dfc7b57c
DevTunechi <tunjidare2@yahoo.com>
2024-09-05 22:34:28 +0100

    Again

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,72 +1,31 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+/* Star Wars Characer - The movie */
 
-const fs = require('fs');
+const req = require('request');
+const baseUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-//Read JSON file
-fs.readFile('return_of_the_jedi.json','utf8', (err, data) => {
-    if (err) {
-	console.error('Error reading file:', err);
-	return;
+req(baseUrl, (err, res, body) => {
+	  if (err) throw err;
+	  if (res.statusCode !== 200) {
+		console.error('status code issue:', res.statusCode);
+		return;
+		}
+
+	  charsChecker(JSON.parse(body).characters, 0);
+});
+
+function charsChecker(chars, idx) {
+	  if (idx >= chars.length) return;
+
+  req(chars[idx], (err, res, body) => {
+	if (err) throw err;
+	
+	if (res.statusCode === 200) {
+		console.log(JSON.parse(body).name);
+	} else {
+		console.error('Status code wrong:', res.statusCode);
+	}
+	charsChecker(chars, idx + 1);
+    });
 }
 
-    // Parse JSON data
-    const movieData = JSON.parse(data);
-
-    console.log(movieData);
-});
-
-const request = require('request');
-
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-// Base URL of the Star Wars API
-const apiUrl = `https://swapi.dev/api/films/3/`;
-
-// Make a GET request to the Star Wars API to get movie details
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data: ${response.statusCode}`);
-    return;
-
-    // Parse the response body as JSON
-    const movieData = JSON.parse(body);
-  }
-
-  // Extract the list of character URLs
-  const characterUrls = movieData.characters;
-
-  // Initialize an empty array to hold promises for fetching character names
-  const characterPromises = [];
-
-  // Iterate over each character URL
-  characterUrls.forEach(url => {
-    // Create a promise to fetch each character's data
-    const characterPromise = new Promise((resolve, reject) => {
-	request(url, (error, response, body) => {
-	if (error) {
-		reject(error);
-	} else {
-		const characterData = JSON.parse(body);
-		resolve(characterData.name);
-		}
-	});
-    });
-
-	  // Add the promise to the array
-	  characterPromises.push(characterPromise);
-  });
-  // Wait for all promises to resolve and then print each character's name
-  Promise.all(characterPromises)
-    .then(characterNames => {
-      characterNames.forEach(name => console.log(name));
-    })
-    .catch(error => {
-      console.error('Error fetching character data:', error);
-    });
-});
