de41a04f2f411da47ccb6ba086857ccf240b4eff
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-09-18 13:12:24 +0100

    Personalised Approach

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Using Personalised Approach """
+
+
+def makeChange(coins, total) -> int:
+    """ Determines the fewest number of coins needed
+        to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    bunch_of_coins = 0
+    for c in coins:
+        if total == 0:
+            break
+        bunch_of_coins += total // c
+        total %= c
+
+    return bunch_of_coins if total == 0 else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,6 @@
+
+
+    GitHub repository: alx-interview
+    Directory: 0x08-making_change
+    File: 0-making_change.py
+
