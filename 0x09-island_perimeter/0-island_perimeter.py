90d66562ef95848ac466df874a8bf9d2bd64fdb1
DevTunechi <tunjidare2@yahoo.com>
2024-09-24 15:10:45 +0100

    Where is the new line coming from?

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,13 +8,11 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Start by adding 4 for the land cell
+                perimeter += 4
 
-                # Check the cell above (i-1, j)
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
 
-                # Check the cell to the left (i, j-1)
                 if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
 
