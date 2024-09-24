0953b937320b7f9511aba3b58a072f19e2f9aa4b
DevTunechi <tunjidare2@yahoo.com>
2024-09-24 15:35:35 +0100

    Tired of the newline

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
+def dfs(grid, r, c):
+    if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+        return 1  # Out of bounds (water)
+    if grid[r][c] == 0:
+        return 1  # Water cell
+    if grid[r][c] == -1:  # Already visited
+        return 0
+
+    grid[r][c] = -1
+
+    return (dfs(grid, r - 1, c) +  # Up
+            dfs(grid, r + 1, c) +  # Down
+            dfs(grid, r, c - 1) +  # Left
+            dfs(grid, r, c + 1))    # Right
+
 def island_perimeter(grid):
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == 1:  # Start DFS from the first land cell
+                return dfs(grid, r, c)
+    return 0
