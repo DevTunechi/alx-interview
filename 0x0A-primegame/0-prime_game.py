c26043b066e0ad17cfbbd5a4c61dd24722647111
DevTunechi <tunjidare2@yahoo.com>
2024-10-01 10:43:36 +0100

    Using Dynamic Programming

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
 
+"""Prime Game"""
+
+
+def sieve_of_eratosthenes(n):
+    """
+    Generate a list of prime numbers up to n.
+    """
+    prime = [True] * (n + 1)
+    prime[0], prime[1] = False, False
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):  # Fix here, increment by `p` not `p*2`
+                prime[i] = False
+        p += 1
+    return prime
+
+
+def count_primes_up_to(prime):
+    """Count the number of primes up to each index."""
+    return [sum(prime[:i+1]) for i in range(len(prime))]
+
+
 def isWinner(x, nums):
+    """Determines winner of prime game."""
     if x == 0 or not nums:
         return None
 
-    max_n = max(nums)
+    n = max(nums)
+    prime = sieve_of_eratosthenes(n)
+    prime_count = count_primes_up_to(prime)
 
-    # Step 1: Sieve of Eratosthenes to find all primes up to max_n
-    sieve = [True] * (max_n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
-    for i in range(2, int(max_n**0.5) + 1):
-        if sieve[i]:
-            for j in range(i*i, max_n + 1, i):
-                sieve[j] = False
-
-    # Step 2: Create a DP table to store the number of prime moves for each n
-    dp = [0] * (max_n + 1)
-
-    # Step 3: Compute the number of primes removed (move count) for each n
-    for i in range(2, max_n + 1):
-        if sieve[i]:
-            # For each prime i, simulate removing all multiples of i
-            for j in range(i, max_n + 1, i):
-                dp[j] += 1
-
-    # Step 4: Determine who wins each game by counting moves
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        # If dp[n] is odd, Ben wins; if even, Maria wins
-        if dp[n] % 2 == 0:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    # Step 5: Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    player1 = sum(prime_count[num] & 1 for num in nums)
+    return "Maria" if player1 > len(nums) // 2 else "Ben"
